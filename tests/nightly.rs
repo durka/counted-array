59557aa81098c30b00d90000bb19b9ae0c8f8e05
Alex Burka <aburka@seas.upenn.edu>
2016-04-13 22:17:18 -0400

    configure tests as bins

--- tests/nightly.rs
+++ tests/nightly.rs
@@ -1,12 +1,11 @@
-#![cfg(feature = "nightly")]
-
 #[macro_use] extern crate counted_array;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
+#[cfg(feature = "nightly")]
 counted_array!(pub lazy_static PLAZY: [u64; _] = [3, 2, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()]);
 
-#[test]
+#[cfg(feature = "nightly")]
 fn lazy_static_reexport() {
     // notice lazy_static was not imported separately in this file
     counted_array!(lazy_static LAZY: [u64; _] = [3, 2, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()]);
@@ -18,4 +17,9 @@ fn lazy_static_reexport() {
     assert_eq!(PLAZY[1], 2);
     assert!(PLAZY[2] > 1400000000);
 }
+
+#[cfg(feature = "nightly")]
+#[test] fn test_lazy_static_reexport() { lazy_static_reexport(); }
+
+fn main() {}
 
--- tests/stable.rs
+++ tests/stable.rs
@@ -1,9 +1,8 @@
-#[macro_use] extern crate lazy_static;
+#[cfg(test)] #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate counted_array;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[test]
 fn private_arrays() {
     counted_array!(const LOCAL_CONST_ARR: [i32; _] = [1, 2, 3]);
     counted_array!(let local_arr: [i32; _] = [4, 5, 6]);
@@ -17,16 +16,16 @@ fn private_arrays() {
 counted_array!(pub const CONST_ARR: [i32; _] = [1, 2, 3]);
 counted_array!(pub static STATIC_ARR: [i32; _] = [7, 8, 9, 10]);
 
-#[test]
 fn public_arrays() {
 
     assert_eq!(CONST_ARR, [1, 2, 3]);
     assert_eq!(STATIC_ARR, [7, 8, 9, 10]);
 }
 
+#[cfg(test)]
 counted_array!(pub lazy_static PLAZY: [u64; _] = [3, 2, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()]);
 
-#[test]
+#[cfg(test)]
 fn lazy_static() {
     counted_array!(lazy_static LAZY: [u64; _] = [3, 2, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()]);
 
@@ -37,4 +36,10 @@ fn lazy_static() {
     assert_eq!(PLAZY[1], 2);
     assert!(PLAZY[2] > 1400000000);
 }
+
+#[test] fn test_private_arrays() { private_arrays(); }
+#[test] fn test_public_arrays() { public_arrays(); }
+#[test] fn test_lazy_static() { lazy_static(); }
+
+fn main() {}
 
